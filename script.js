9cb2fde4e9d606ddda9fc25ed02690d0016a9e46
ROHAN DAS <61402717+rohanbabai2000@users.noreply.github.com>
2025-07-28 09:06:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	arr.forEach(func);
+	function func(i, arr){
+		if(arr[i]<arr[i+1]){
+			let temp=arr[i];
+			arr[i]=arr[i+1];
+			arr[i+1]=temp;
+		}
+	}
+	
+		let n=arr.length;
+		let s=0;
+		for(let i=0;i<n;i+2){
+			s=s+(arr[i]+arr[i+1]);
+		}
+		return s;
+	}
   
 }
 
